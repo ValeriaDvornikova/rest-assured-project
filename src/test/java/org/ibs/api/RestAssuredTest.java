e9271343898e00ab372dae3c0927b1f9d22e7025
ValeriaDvornikova <valeri2001.02@gmail.com>
2024-04-22 21:18:36 +0300

    refactor tests

--- src/test/java/org/ibs/api/RestAssuredTest.java
+++ src/test/java/org/ibs/api/RestAssuredTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class RestAssuredTest {
     private static final String URI = "http://localhost:8080";
     private static final int INIT_SIZE = 4;
     private final Map<String, String> withoutContentHeaders = Map.of("connection", "keep-alive", "content-length", "0");
 
+    private static Map<String,String> cookies;
     @BeforeEach
     void setUp() {
         Specifications.installSpecifications(Specifications.requestSpec(URI),
@@ -22,16 +26,48 @@ public class RestAssuredTest {
     }
     @Test
     void addProductTest() {
+        List<GoodsData> beforePostList = given()
+                .when()
+                .get("/api/food")
+                .then()
+                .log().all()
+                .extract()
+                .body()
+                .jsonPath()
+                .getList("", GoodsData.class);
+
         GoodsData sendData = new GoodsData("Джекфрут", "FRUIT", true);
 
-        given()
+        cookies = given()
                 .body(sendData)
                 .header("Content-Type", "application/json")
                 .when()
                 .post("/api/food")
                 .then().log().all()
                 .assertThat()
-                .headers(withoutContentHeaders);
+                .headers(withoutContentHeaders)
+                .extract()
+                .cookies();
+
+        List<GoodsData> afterPostList = given()
+                .when()
+                .cookies(cookies)
+                .get("/api/food")
+                .then()
+                .log().all()
+                .extract()
+                .body()
+                .jsonPath()
+                .getList("", GoodsData.class);
+
+        assertAll(
+                () -> assertThat(afterPostList, hasSize(INIT_SIZE + 1)),
+                () -> assertThat(afterPostList, hasItem(allOf(
+                        hasProperty("name", equalTo(sendData.getName())),
+                        hasProperty("type", equalTo(sendData.getType())),
+                        hasProperty("exotic", equalTo(sendData.getExotic())
+                ))))
+        );
     }
 
     @Test
@@ -41,8 +77,8 @@ public class RestAssuredTest {
                 .post("/api/data/reset")
                 .then()
                 .log().all()
-                .assertThat()
-                .headers(withoutContentHeaders);
+                .assertThat();
+                //.headers(withoutContentHeaders);
     }
 
     @Test
